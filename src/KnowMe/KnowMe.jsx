4675c834c968bd230aa086f55e050f2a078f3992
diego59x <diegoalvarez1237@hotmail.com>
2021-07-01 22:20:06 -0600

    upload page knowme in small devices

--- src/KnowMe/KnowMe.jsx
+++ src/KnowMe/KnowMe.jsx
@@ -12,29 +12,33 @@ export default function KnowMe() {
   return (
     <div>
       <div className="row ">
-        <div className="col-4 circleBack">
+        <div className="col-xs-6 circleBack">
           <CircleSecondary
             header="Back"
             onClick={goBack}
             classe="boton"
           />
         </div>
-        <div className="col-6 headerMe">
+        <div className="col-xs-6 headerMe">
           Know Me
         </div>
-
       </div>
-      <div className="row ">
-        <div className="col-6">
+      <div className="row">
+        <div className="col-xs-12 ">
           <div className="cartMe ">
             Hi, how i said before i&#39;m a junior web devloper. I like both front-end and back-end,
             but also make videogames.
             I have made some games for my own and another ones in my university.
-            {/* <img src={Scendere} alt="" className="imageProject" href="https://github.com/diego59x/Scendere.git" /> */}
           </div>
-          <div className="col-6">
-            <img src={me} alt="" className="imgMe" />
-          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-xs-12 ">
+          <img src={me} alt="" className="imgMe" />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-xs-12">
           <div className="cartMe ">
             I consider myself a fresh person, friendly, determined, persistent,
             quiet, but also outgoing. I like to analyze a problem from all its perspectives,
