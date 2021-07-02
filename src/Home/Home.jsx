e519402473d2a63169c77895a28cae3e720d62b5
diego59x <diegoalvarez1237@hotmail.com>
2021-07-01 20:41:01 -0600

    home page ready

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -9,12 +9,12 @@ export default function Home() {
   const KnowMe = () => {
     history.push('/KnowMe')
   }
-  // const Projects = () => {
-  //   history.push('/Projects')
-  // }
+  const Projects = () => {
+    history.push('/Projects')
+  }
   return (
     <div>
-      <div className="row aber">
+      <div className="nav nav-justified">
         <div className="col-xs-10 col-sm-12 col-md-12 col-lg-4  ">
           <Circle
             text="Hi, it&#39;s Diego. I&#39;m a Junior Developer that born in 2001 so, you can guess my age"
@@ -34,35 +34,35 @@ export default function Home() {
             Or let my code speak
           </div>
         </div>
-        {/*
-        <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-          <div className=" circleHome">
-            <CircleSecondary
-              header="Contact"
-              classe="boton"
-            />
+      </div>
+      <div className="row">
+        <div className="col-xs-4 circleHome">
+          <CircleSecondary
+            header="Contact"
+            classe="boton"
+          />
+        </div>
+        <div className="col-xs-4 HomeText ">
+          <div className="texts1">
+            Call Me
+            {/* Email: alv19498@uvg.edu.gt
+            Github: diego59x */}
           </div>
-          <div className="HomeText ">
-            <div className="texts1">
-              Email: alv19498@uvg.edu.gt
-              Github: diego59x
-            </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-xs-4 circleHome">
+          <CircleSecondary
+            header="Projects"
+            onClick={Projects}
+            classe="boton"
+          />
+        </div>
+        <div className="col-xs-4  HomeText ">
+          <div className="texts1">
+            See what I have made
           </div>
-          <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-            <div className=" circleHome">
-              <CircleSecondary
-                header="Projects"
-                onClick={Projects}
-                classe="boton"
-              />
-            </div>
-            <div className="HomeText ">
-              <div className="texts1">
-                See what I have made
-              </div>
-            </div>
-          </div>
-        </div> */}
+        </div>
       </div>
     </div>
   )
